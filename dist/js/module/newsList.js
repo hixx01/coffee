d26268ed44ac356862032caa519662e5daad128d
谢星 <xiexing@MacBookAir.local>
2017-09-05 17:24:56 +0800

    coffee

--- dist/js/module/newsList.js
+++
@@ -1,1 +0,0 @@
-!function(e){e.ajaxSetup({error:function(){return alert("调用接口失败"),!1}}),e.getJSON("http://www.easy-mock.com/mock/59841572a1d30433d8532e72/message/list/message/list",function(t){var a=e("#notice-template").html(),m=Handlebars.compile(a)(t);e("#main").html(m)})}(jQuery);
